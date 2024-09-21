29ed655c5bb4601876d28f9f658fcb8d84a9342f
Jack Leow <jleow@tubi.tv>
2024-09-21 10:48:57 -0700

    Moved function up.

--- reactive_word_cloud/service.py
+++ reactive_word_cloud/service.py
@@ -12,6 +12,35 @@ from reactive_word_cloud.model import *
 L = TypeVar('L')
 R = TypeVar('R')
 
+def user_input(kafka_conf: dict[str, str], topic_name: str) -> Observable[SenderAndText]:
+    def consume_message(consumer: Consumer) -> rx.Observable[Message]:
+        msg: Message | None = consumer.poll(timeout=1.0)
+        return rx.just(msg) if msg is not None else rx.empty()
+
+    def not_error(msg: Message) -> bool:
+        return not msg.error()
+
+    def extract_chat_message(msg: Message) -> Observable[SenderAndText]:
+        value: str | bytes | None = msg.value()
+        sender_text: SenderAndText | None
+        match value:
+            case str():
+                sender_text = SenderAndText.from_json(value)
+            case bytes():
+                sender_text = SenderAndText.from_json(value.decode('utf-8'))
+            case _:
+                sender_text = None
+        if sender_text is not None:
+            print(f'consumed: {sender_text.to_json()}')
+        return rx.empty() if sender_text is None else rx.just(sender_text)
+
+    consumer: Consumer = Consumer(kafka_conf)
+    consumer.subscribe([topic_name])
+    return rx.repeat_value(consumer) \
+        >> ops.concat_map(consume_message) \
+        >> ops.filter(not_error) \
+        >> ops.concat_map(extract_chat_message)
+
 max_words_per_sender: int = 3
 min_word_len: int = 3
 max_word_len: int = 15
@@ -118,35 +147,6 @@ stop_words: set[str] = {
     'yourself',
     'yourselves'
 }
-
-def user_input(kafka_conf: dict[str, str], topic_name: str) -> Observable[SenderAndText]:
-    def consume_message(consumer: Consumer) -> rx.Observable[Message]:
-        msg: Message | None = consumer.poll(timeout=1.0)
-        return rx.just(msg) if msg is not None else rx.empty()
-
-    def not_error(msg: Message) -> bool:
-        return not msg.error()
-
-    def extract_chat_message(msg: Message) -> Observable[SenderAndText]:
-        value: str | bytes | None = msg.value()
-        sender_text: SenderAndText | None
-        match value:
-            case str():
-                sender_text = SenderAndText.from_json(value)
-            case bytes():
-                sender_text = SenderAndText.from_json(value.decode('utf-8'))
-            case _:
-                sender_text = None
-        if sender_text is not None:
-            print(f'consumed: {sender_text.to_json()}')
-        return rx.empty() if sender_text is None else rx.just(sender_text)
-
-    consumer: Consumer = Consumer(kafka_conf)
-    consumer.subscribe([topic_name])
-    return rx.repeat_value(consumer) \
-        >> ops.concat_map(consume_message) \
-        >> ops.filter(not_error) \
-        >> ops.concat_map(extract_chat_message)
 
 def normalize_text(sender_text: SenderAndText) -> SenderAndText:
     return SenderAndText(
