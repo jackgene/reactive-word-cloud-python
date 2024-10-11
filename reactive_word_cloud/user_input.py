f0a0efd30da2abc859b65d607093f6d892d3fbc3
Jack Leow <jackgene@gmail.com>
2024-10-11 09:10:55 -0700

    Added more complete aiokafka type stubs, include them in Pyright, upgrade Pyright.

--- reactive_word_cloud/user_input.py
+++ reactive_word_cloud/user_input.py
@@ -1,6 +1,6 @@
 import asyncio
 import logging
-from asyncio import Task
+from asyncio import Semaphore, Task
 from typing import Literal
 
 import reactivex as rx
@@ -20,29 +20,51 @@ from reactive_word_cloud.model import SenderAndText
 
 def from_kafka(config: KafkaConfig) -> Observable[SenderAndText]:
     def consume_messages(observer: ObserverBase[bytes], _: SchedulerBase | None) -> DisposableBase:
-        consumer: AIOKafkaConsumer = AIOKafkaConsumer(
+        consumer: AIOKafkaConsumer[bytes, bytes] = AIOKafkaConsumer(
             *config.topic_names,
             bootstrap_servers=config.bootstrap_servers,
             group_id=config.group_id,
             enable_auto_commit=config.enable_auto_commit,
-            auto_offset_reset=config.auto_offset_reset
+            auto_offset_reset=config.auto_offset_reset,
         )
+        done: Semaphore = Semaphore(0)
         async def consume():
-            await consumer.start()
             try:
-                msg: ConsumerRecord[bytes, bytes]
-                async for msg in consumer:
-                    if msg.value is not None:
-                        observer.on_next(msg.value)
+                async with consumer:
+                    await_done_task: Task[Literal[True]] = asyncio.create_task(done.acquire())
+                    while True:
+                        msg_or_dones: set[Task[ConsumerRecord[bytes, bytes]] | Task[Literal[True]]]
+                        pendings: set[Task[ConsumerRecord[bytes, bytes]] | Task[Literal[True]]]
+                        msg_or_dones, pendings = await asyncio.wait(
+                            [asyncio.create_task(consumer.getone()), await_done_task],
+                            return_when=asyncio.FIRST_COMPLETED
+                        )
+                        msg_or_done: ConsumerRecord[bytes, bytes] | bool = msg_or_dones.pop().result()
+                        if isinstance(msg_or_done, ConsumerRecord) and msg_or_done.value is not None:
+                            observer.on_next(msg_or_done.value)
+                        elif msg_or_done == True:
+                            observer.on_completed()
+                            for pending in pendings:
+                                pending.cancel()
+                            break
+                    msg: ConsumerRecord[bytes, bytes]
+                    async for msg in consumer:
+                        if msg.value is not None:
+                            observer.on_next(msg.value)
             except Exception as e:
+                logging.error(f'error while receiving Kafka user input: {e}')
                 observer.on_error(e)
-            finally:
-                await consumer.stop()
-                observer.on_completed()
-        asyncio.create_task(consume())
+        consumer_task: Task[None] = asyncio.create_task(consume())
 
         def dispose():
-            asyncio.run_coroutine_threadsafe(consumer.stop(), asyncio.get_running_loop())
+            done.release()
+            def await_connection_close():
+                from time import sleep
+                while not (consumer_task.done() and consumer._closed): # type: ignore
+                    sleep(0.05)
+                logging.info('user input consumer WebSockets connection closed')
+            # Python seems to wait for this to complete before shutting down
+            asyncio.run_coroutine_threadsafe(asyncio.to_thread(await_connection_close), asyncio.get_running_loop())
 
         return Disposable(dispose)
 
@@ -57,30 +79,30 @@ def from_kafka(config: KafkaConfig) -> Observable[SenderAndText]:
 
 def from_websockets(config: WebSocketsConfig) -> Observable[SenderAndText]:
     def consume_messages(observer: ObserverBase[str], _: SchedulerBase | None) -> DisposableBase:
-        done: asyncio.Semaphore = asyncio.Semaphore(0)
         ws_conn: ws_client.connect = ws_client.connect(config.url)
+        done: Semaphore = Semaphore(0)
         async def consume():
             try:
                 conn: Connection
                 async with ws_conn as conn:
                     await_done_task: Task[Literal[True]] = asyncio.create_task(done.acquire())
                     while True:
-                        msgs: set[Task[str | bytes] | Task[Literal[True]]]
+                        msg_or_dones: set[Task[str | bytes] | Task[Literal[True]]]
                         pendings: set[Task[str | bytes] | Task[Literal[True]]]
-                        msgs, pendings = await asyncio.wait(
+                        msg_or_dones, pendings = await asyncio.wait(
                             [asyncio.create_task(conn.recv()), await_done_task],
                             return_when=asyncio.FIRST_COMPLETED
                         )
-                        msg: str | bytes | bool = msgs.pop().result()
-                        if isinstance(msg, str):
-                            observer.on_next(msg)
-                        elif msg == True:
+                        msg_or_done: str | bytes | bool = msg_or_dones.pop().result()
+                        if isinstance(msg_or_done, str):
+                            observer.on_next(msg_or_done)
+                        elif msg_or_done == True:
                             observer.on_completed()
                             for pending in pendings:
                                 pending.cancel()
                             break
             except Exception as e:
-                logging.error(f'error while receiving websocket messages: {e}')
+                logging.error(f'error while receiving WebSockets user input: {e}')
                 observer.on_error(e)
         consumer_task = asyncio.create_task(consume())
 
